9a73a4e0132e1c319f46e8962b764f55d61046e8
ASKOUR <prizzaflutter@gmail.com>
2024-06-15 15:59:15 +0100

    askour file is added succefully

---
+++ lib/ui/main_pages/skour/ask.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class askour extends StatefulWidget {
+  const askour({super.key});
+
+  @override
+  State<askour> createState() => _askourState();
+}
+
+class _askourState extends State<askour> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
